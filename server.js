e6c9b39a114e4995bd2342f9ae482cd8503ada85
Asrar Memon <asrar.memon@i-link.co.in>
2020-04-21 20:20:33 +0530

    chanes

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', ['http://localhost:4200']);
+    // res.setHeader('Access-Control-Allow-Origin', 'http://13.126.185.9:3000');
+
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
